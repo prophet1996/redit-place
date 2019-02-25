1d3c7f8cb6e45c06e33627f5e5927776a22d080c
ishank <ishank.web.dev@gmail.com>
2019-02-26 02:19:41 +0530

    Updates

---
+++ precache-manifest.9bee877326376c42c1c878066edbcaf0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b794b22164d626a8d85f",
+    "url": "/redit-place/static/js/runtime~main.b794b221.js"
+  },
+  {
+    "revision": "83788cdc4eb7e23cbdce",
+    "url": "/redit-place/static/js/main.83788cdc.chunk.js"
+  },
+  {
+    "revision": "907aad528399a8354357",
+    "url": "/redit-place/static/js/2.907aad52.chunk.js"
+  },
+  {
+    "revision": "83788cdc4eb7e23cbdce",
+    "url": "/redit-place/static/css/main.32987235.chunk.css"
+  },
+  {
+    "revision": "77ba7b70fb7e108a30d197822522cf99",
+    "url": "/redit-place/index.html"
+  }
+];
